1613884d8836a2c63bea85008082502d31b45b95
anonymous-cloud <57187026+anonymous-cloud@users.noreply.github.com>
2025-05-22 01:52:07 +0530

    creting databse schema for publicer

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,9 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const signup = async(req,res)=>{
     try {
@@ -19,6 +23,8 @@ const signup = async(req,res)=>{
       const newUser = new User({name,email,password : hasedPassword});
 
       await newUser.save();
+
+
       res.status(201).json({message : "user created sussesfully"});
 
 
@@ -43,7 +49,9 @@ const login = async (req,res)=>{
             return res.status(400).json({message : "invalid credentials"});
         }
 
-        res.status(200).json({message : "login successful"})
+        const token = jwt.sign({id:dataUser._id},JWT_SECRET,{expiresIn : "24h"});
+
+        res.status(200).json({message : "login successful", token})
     }catch(err){
         console.log(err)
         return res.status(500).json({err : err.message});
